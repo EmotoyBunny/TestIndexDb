485ab8f309d0218013280398f43e0ca1e40673fa
EmotoyBunny <sonka.zaichkina@mail.ru>
2023-03-13 09:52:31 +0300

    test на 100000 строк

--- test-index-db/src/_components/PouchDbComponent/PouchDbComponent.jsx
+++ test-index-db/src/_components/PouchDbComponent/PouchDbComponent.jsx
@@ -5,6 +5,7 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import {Grid, Typography, Button, TextField} from '@mui/material';
 import {withTheme} from "@mui/styles";
 import {PouchDb} from "../Pouchdb"
+import {textConst} from "./text";
 
 /**
  * Компонент авторизации
@@ -18,9 +19,13 @@ class PouchDbComponent extends React.Component {
             user: {
                 username: "",
                 password: "",
-                ownId: "",
-                data: []
             },
+            i: 0,
+            j: 0,
+            idArray: [],
+            log: [],
+            data: {},
+            ownId: "",
             pageHeightClient: 600,
         };
     }
@@ -33,7 +38,73 @@ class PouchDbComponent extends React.Component {
         this.setState({[e.target.name]: e.target.value})
     };
 
-    // Генерация своего id
+    startTest = () => {
+        console.log("Start");
+        this.handleAddNotId();
+    };
+
+    showLog = () => {
+        console.log(this.state.log)
+    };
+
+
+    handleAddNotId = () => {
+        let {log, i} = this.state;
+        const startAdd = new Date().getTime();
+        if (i < 100000) {
+            PouchDb.post({
+                title: textConst.text,
+            }).then((res) => {
+                const endAdd = new Date().getTime();
+                log.push({
+                    name: "add",
+                    time: (endAdd - startAdd)
+                });
+                return res;
+            }).then((res) => {
+                const startTake = new Date().getTime();
+                PouchDb.get(res.id).then((doc) => {
+                    const endTake = new Date().getTime();
+                    log.push({
+                        name: "take",
+                        time: (endTake - startTake)
+                    });
+                    return doc;
+                }).then((doc) => {
+                    const startEdit = new Date().getTime();
+                    PouchDb.put({
+                        _id: doc._id,
+                        _rev: doc._rev,
+                        title: textConst.text + "1",
+                    }).then(() => {
+                        const endEdit = new Date().getTime();
+                        log.push({
+                            name: "edit",
+                            time: (endEdit - startEdit)
+                        });
+                        this.setState({log: log, i: i + 1}, () => {
+                            if (i % 1000 === 0) {
+                                console.log(i)
+                            } else if (i === (100000 - 1)) {
+                                this.handleShowAll();
+                                console.log("Finish")
+                            }
+                            this.handleAddNotId()
+                        })
+                    }).catch(function (err) {
+                        console.log(err);
+                    });
+                }).catch(function (err) {
+                    console.log(err);
+                });
+            }).catch(function (err) {
+                console.log(err);
+            });
+        }
+    };
+
+
+// Генерация своего id
     handleAddId = () => {
         let idRes = this.state.ownId;
         if (idRes === "") {
@@ -52,51 +123,47 @@ class PouchDbComponent extends React.Component {
         });
     };
 
-    // Генерация автоматического id
-    handleAddNotId = () => {
-        PouchDb.post({
-            title: JSON.stringify({param1: 10, param2: "asd"}),
-            nameMethod: "nameMethod",
-        }).then((response) => {
-            console.log(response);
-            this.handleShow(this.state.ownId);
-            this.handleShowAll();
-        }).catch(function (err) {
-            console.log(err);
-        });
-    };
-
 
     handleShow = () => {
-        PouchDb.get(this.state.ownId).then(function (doc) {
-            console.log(doc)
-        }).catch(function (err) {
+        PouchDb.get(this.state.ownId).catch(function (err) {
             console.log(err);
         });
     };
 
 
-    handleDelete = (id) => {
-        let deleteId = id;
-        if (!deleteId) {
-            deleteId = this.state.ownId
-        }
-        PouchDb.get(deleteId).then(function (doc) {
-            return PouchDb.remove(doc);
-        }).then((result) => {
-            console.log(result);
-            this.handleShowAll();
-        }).catch(function (err) {
-            console.log(err);
-        });
+    handleDelete = () => {
+        let {j, data} = this.state;
+        if (data && data.rows.length > 0 && j < data.rows.length)
+            PouchDb.get(this.state.idArray[j]).then((doc) => {
+                return PouchDb.remove(doc).then(() => {
+                    this.setState({j: j + 1}, () => {
+                        if (j === data.rows.length - 1) {
+                            this.handleShowAll();
+                            console.log("deleteFinish");
+                        } else if (j % 1000 === 0) {
+                            console.log(j)
+                        }
+                        this.handleDelete();
+                    })
+                });
+            }).catch(function (err) {
+                console.log(err);
+            });
     };
 
 
     handleDeleteAll = () => {
-        for (let i = 0; i < this.state.data.rows.length; i++) {
-            this.handleDelete(this.state.data.rows[i].id)
+        if (this.state.data && this.state.data.rows.length > 0) {
+            console.log("deleteStart");
+            for (let i = 0; i < this.state.data.rows.length; i++) {
+                let {idArray} = this.state;
+                idArray.push(this.state.data.rows[i].id);
+                this.setState({idArray: idArray})
+            }
+            this.handleDelete()
+        } else {
+            console.log("Элементов нет");
         }
-        this.handleShowAll();
     };
 
     handleEdit = () => {
@@ -121,7 +188,7 @@ class PouchDbComponent extends React.Component {
             include_docs: true,
             attachments: true
         }).then((result) => {
-            console.log(result);
+            console.log(result)
             this.setState({data: result})
         }).catch(function (err) {
             console.log(err);
@@ -141,7 +208,70 @@ class PouchDbComponent extends React.Component {
                               paddingLeft: "10px",
                               paddingRight: "10px"
                           }}>
-                        <Grid item xs={12}>
+                        <Grid xs={12} item>
+                            <Button
+                                onClick={this.startTest}
+                                style={{
+                                    height: "50px",
+                                    minWidth: "300px",
+                                    maxWidth: "300px"
+                                }}
+                                fullWidth
+                                color={"secondary"}
+                                variant="contained"
+                            >
+                                <Typography sx={{
+                                    fontSize: "20px",
+                                    color: "#fff",
+                                    textTransform: "none"
+                                }}>
+                                    Запустить
+                                </Typography>
+                            </Button>
+                        </Grid>
+                        <Grid xs={12} item>
+                            <Button
+                                onClick={this.showLog}
+                                style={{
+                                    height: "50px",
+                                    minWidth: "300px",
+                                    maxWidth: "300px"
+                                }}
+                                fullWidth
+                                color={"secondary"}
+                                variant="contained"
+                            >
+                                <Typography sx={{
+                                    fontSize: "20px",
+                                    color: "#fff",
+                                    textTransform: "none"
+                                }}>
+                                    Вывести результаты
+                                </Typography>
+                            </Button>
+                        </Grid>
+                        <Grid xs={12} item>
+                            <Button
+                                onClick={this.handleDeleteAll}
+                                style={{
+                                    height: "50px",
+                                    minWidth: "300px",
+                                    maxWidth: "300px"
+                                }}
+                                fullWidth
+                                color={"secondary"}
+                                variant="contained"
+                            >
+                                <Typography sx={{
+                                    fontSize: "20px",
+                                    color: "#fff",
+                                    textTransform: "none"
+                                }}>
+                                    Удалить все элементы
+                                </Typography>
+                            </Button>
+                        </Grid>
+                        {/*<Grid item xs={12}>
                             <TextField
                                 name={"ownId"}
                                 style={{width: "200px", height: "50px"}}
@@ -297,8 +427,8 @@ class PouchDbComponent extends React.Component {
                                     Удалить все элементы
                                 </Typography>
                             </Button>
-                        </Grid>
-                        {this.state.data && this.state.data.rows.map((item) => {
+                        </Grid>*/}
+                        {/*{this.state.data && this.state.data.rows.map((item) => {
                             return (
                                 <Grid xs={12} item key={item.id}>
                                     <div style={{width: "100%"}}>
@@ -314,7 +444,7 @@ class PouchDbComponent extends React.Component {
                                     </div>
                                 </Grid>
                             )
-                        })}
+                        })}*/}
                     </Grid>
                 </form>
             </div>);
